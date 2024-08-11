e9648f6206edcb6f5828dc792f6006f79cd09100
auto-assai <videl001@gmail.com>
2024-08-11 15:23:11 +0300

    Изменил текстовку метода main, команда println

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class Test {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("И даже когда кажется, что не за чем, не растекайся по дивану майонезычем!");
   }
   
 }
